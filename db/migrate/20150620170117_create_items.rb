9473e433f08b3e271597f57205b531dfc9a8d443
aoripov <akbarzoripov@gmail.com>
2015-06-20 23:04:29 +0200

    Add price field to item

--- db/migrate/20150620170117_create_items.rb
+++ db/migrate/20150620170117_create_items.rb
@@ -2,6 +2,8 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
+      t.string :image
+      t.float :price
       t.belongs_to :user, index: true
       t.timestamps null: false
     end
