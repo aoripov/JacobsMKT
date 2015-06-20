5519af55016a1a86ddd8fc5d54bc1b139ae31785
Felix Schmoll <eggi.innovations@gmail.com>
2015-06-20 20:10:42 +0200

    Enabled login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'signup'  => 'users#new'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
