26ef0bc381ace88adab883656530216f1c5bb94d
Felix Schmoll <eggi.innovations@gmail.com>
2015-06-21 01:28:31 +0200

    Corrected logging out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get '/logout' => 'sessions#logout'
   get '/items' => 'items#index'
   get '/items/new' => 'items#new'
   post '/items' => 'items#create'
