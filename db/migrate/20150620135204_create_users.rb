54dc80f442b2f3004ca155b5e5c64872ed0fbb35
aoripov <akbarzoripov@gmail.com>
2015-08-19 19:39:08 +0200

    Add username and token to users

--- db/migrate/20150620135204_create_users.rb
+++ db/migrate/20150620135204_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
+      t.string :username
       t.string :email
-
+      t.string :token
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,24 +14,22 @@
 ActiveRecord::Schema.define(version: 20150706164137) do
 
   create_table "items", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.string   "image",              limit: 255
-    t.text     "description",        limit: 65535
-    t.float    "price",              limit: 24
-    t.integer  "user_id",            limit: 4
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.string   "photo_file_name",    limit: 255
-    t.string   "photo_content_type", limit: 255
-    t.integer  "photo_file_size",    limit: 4
-    t.datetime "photo_updated_at"
+    t.string   "name",        limit: 255
+    t.string   "image",       limit: 255
+    t.text     "description", limit: 65535
+    t.float    "price",       limit: 24
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
+    t.string   "username",        limit: 255
     t.string   "email",           limit: 255
+    t.string   "token",           limit: 255
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
