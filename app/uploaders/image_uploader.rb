9512a02f518434021c68776461c6c0c9b48b19a4
Felix Schmoll <eggi.innovations@gmail.com>
2015-09-02 13:47:33 +0200

    Changed image storage directory

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,10 +5,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   include CarrierWave::MiniMagick
 
-  def store_dir
-    'public/images'
-  end
-
   #image types that are allowed to be uploaded
   def extension_white_list
     %w(jpg jpeg gif png)
@@ -23,9 +19,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  def store_dir
+   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
